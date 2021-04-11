05ed2b6abc09db5596385bb013e68cd710a24ecc
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2021-04-10 20:46:11 -0500

    course details part 1

--- App.js
+++ App.js
@@ -1,45 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
-import CourseList from './components/CourseList';
-
-const Banner = ({title}) => (
-  <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
-);
+import React from 'react';
+import ScheduleScreen from './screens/ScheduleScreen';
 
 const App = () => {
-  const [schedule, setSchedule] = useState({title: '', courses: []});
-
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
-
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    }
-    fetchSchedule();
-  }, [])
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Banner title = {schedule.title}/>
-      <CourseList courses={schedule.courses}/>
-    </SafeAreaView>
+    <ScheduleScreen />
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 20,
-  },
-  bannerStyle: {
-    color: '#888',
-    fontSize: 32,
-  },
-});
+};
 
 export default App;
